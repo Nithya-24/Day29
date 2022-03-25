5ae7e096a511aa88e7b4b999126d3052bee44672
Nithya-24 <nithyaramadoz@gmail.com>
2022-03-25 23:22:24 +0530

    Test case for Check the Records is done

--- src/test/java/day29Test/TestCase.java
+++ src/test/java/day29Test/TestCase.java
@@ -1,11 +1,21 @@
 package day29Test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCase {
+	StateCensusAnalyser analyser;
+
+	@Before
+	public void initialization() {
+		analyser = new StateCensusAnalyser();
+	}
+
 	@Test
-	public void testCensusAnalyser() {
-		StateCensusAnalyser analyser = new StateCensusAnalyser();
+	public void testRecordMatch() {
 		analyser.loadData();
+		assertEquals(true, analyser.checkData());
 	}
 }
