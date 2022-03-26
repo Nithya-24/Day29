8d6bd87b93c4dbc7b1b4f1a82c9d02377ada50ce
Nithya-24 <nithyaramadoz@gmail.com>
2022-03-26 22:02:40 +0530

    Delimiter Incorrest Returns Custom exception done

---
+++ src/main/java/day29Test/InvalidDelimiter.java
@@ -0,0 +1,18 @@
+package day29Test;
+
+public class InvalidDelimiter extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidDelimiter() {
+		super();
+	}
+	
+	public InvalidDelimiter(String message) {
+		super(message);
+	}
+
+}
--- src/main/java/day29Test/StateCensusAnalyser.java
+++ src/main/java/day29Test/StateCensusAnalyser.java
@@ -14,7 +14,7 @@ import com.opencsv.exceptions.CsvValidationException;
 		/**
 		 *  Method to load the data and display
 		 */
-		public void loadData(String filePath) throws InvalidFile { 
+		public void loadData(String filePath) throws InvalidFile, InvalidDelimiter { 
 
 			try {
 				/**
